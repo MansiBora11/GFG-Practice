a531886e6e6562bf0438ed2b6449f21d3f824a22
MansiBora11 <boramansi118@gmail.com>
2022-11-10 19:25:50 +0530

    Infix to Postfix

--- Infix to Postfix.cpp
+++ Infix to Postfix.cpp
@@ -7,72 +7,51 @@ infix expression to postfix*/
 using namespace std;
 
 // Function to return precedence of operators
-int prec(char c)
-{
-	if (c == '^')
-		return 3;
-	else if (c == '/' || c == '*')
-		return 2;
-	else if (c == '+' || c == '-')
-		return 1;
-	else
-		return -1;
-}
-
-// The main function to convert infix expression
-// to postfix expression
-void infixToPostfix(string s)
-{
-
-	stack<char> st; // For stack operations, we are using
-					// C++ built in stack
-	string result;
-
-	for (int i = 0; i < s.length(); i++) {
-		char c = s[i];
-
-		// If the scanned character is
-		// an operand, add it to output string.
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
-			|| (c >= '0' && c <= '9'))
-			result += c;
-
-		// If the scanned character is an
-		// ‘(‘, push it to the stack.
-		else if (c == '(')
-			st.push('(');
-
-		// If the scanned character is an ‘)’,
-		// pop and to output string from the stack
-		// until an ‘(‘ is encountered.
-		else if (c == ')') {
-			while (st.top() != '(') {
-				result += st.top();
-				st.pop();
-			}
-			st.pop();
-		}
-
-		// If an operator is scanned
-		else {
-			while (!st.empty()
-				&& prec(s[i]) <= prec(st.top())) {
-				result += st.top();
-				st.pop();
-			}
-			st.push(c);
-		}
-	}
-
-	// Pop all the remaining elements from the stack
-	while (!st.empty()) {
-		result += st.top();
-		st.pop();
-	}
-
-	cout << result << endl;
-}
-
+int priority(char ch){
+        if (ch == '+' or ch == '-')  return 1;
+        else if (ch == '/' or ch == '*') return 2;
+        else if (ch == '^')  return 3;
+        
+        return 0;
+    }
+    string infixToPostfix(string s) {
+        
+        string ans = "";
+        stack <char> st;
+        
+        for (int i = 0; i<s.length(); i++){
+            char ch = s[i];
+            if ( ('a' <= ch and ch <= 'z') or ('A' <= ch and ch <= 'Z') ){
+                ans += ch;
+                
+            }else if (ch == '('){
+                st.push(ch);
+                
+            }
+            else if (ch == ')'){
+                while (st.top() != '('){
+                    ans += st.top();
+                    st.pop();
+                }
+                st.pop();
+                
+            }
+            else {
+                while (!st.empty() and (priority(st.top()) >=priority(ch))){
+                    ans += st.top();
+                    st.pop();
+                }
+                st.push(ch);
+            }
+        }
+        while (!st.empty()) {
+            ans+= st.top();
+            st.pop();
+        }
+        
+        return ans;
+        
+    }
 // Driver's code
 int main()
 {

5ffd63775693d561a289bc4ff6c933eb685d2bd2
MansiBora11 <boramansi118@gmail.com>
2022-11-14 22:23:59 +0530

    Remove duplicate element from sorted Linked List

---
+++ Remove duplicate element from sorted Linked List.cpp
@@ -0,0 +1,17 @@
+// Remove duplicate element from sorted Linked List
+
+Node *removeDuplicates(Node *head)
+{
+  Node*curr=head;
+  while(curr!=NULL && curr->next!=NULL){
+      if(curr->data==curr->next->data){
+          Node *temp=curr->next;
+          curr->next=curr->next->next;
+          delete(temp);
+      }
+      else
+        curr=curr->next;
+      
+  }
+  return head;
+}
